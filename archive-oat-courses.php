488b31c9fc6433c272a222eb77817c6e3211db6e
Cheryl Cam <cherylcam97@gmail.com>
2019-09-25 13:49:27 -0700

    theme color and fonts added

--- archive-oat-courses.php
+++ archive-oat-courses.php
@@ -19,33 +19,39 @@ get_header();
 				<h1 class="page-title">Courses</h1>
 			</header><!-- .page-header -->
 
+			<section>
 			<?php while ( have_posts() ) :
-					the_post();
-					the_title(); ?>
-					
-				<?php if( function_exists('get_field') ):
-						$file = get_field('course_outline'); ?>
+					the_post(); ?>
+					<div class="course-wrap">
+						<!-- <div class="course-heading"> -->
+						<h1 class="course-heading"><?php the_title(); ?>
+							<?php if( function_exists('get_field') ):
+								$file = get_field('course_outline'); ?>
 
-					<?php if( get_field('credits') ): ?>
-							<span>Credits: <?php the_field('credits') ?></span>
-					<?php endif; ?>
+							<?php if( get_field('credits') ): ?>
+									<span class="credits">Credits: <?php the_field('credits'); ?></span>
+							<?php endif; ?>
+						</h1>
+						
+						<!-- </div>  -->
+						<!---end course heading--->
 
-					<?php if( get_field('course_description') ): ?>
-							<p><?php the_field('course_description') ?></p>
-					<?php endif; ?>
+							<?php if( get_field('course_description') ): ?>
+									<p><?php the_field('course_description'); ?></p>
+							<?php endif; ?>
 
-					<?php if( $file ): ?>
-							<a href="<?php echo $file['url']; ?>">Download OAT <?php echo ucwords(strtolower(substr(strstr(get_the_title()," "), 1))); ?> Course Outline</a>
-					<?php endif; ?>
+							<?php if( $file ): ?>
+									<a href="<?php echo $file['url']; ?>">Download OAT <?php echo ucwords(strtolower(substr(strstr(get_the_title()," "), 1))); ?> Course Outline</a>
+							<?php endif; ?>
 
-					<?php if( get_field('course_image') ): 
-							echo wp_get_attachment_image( get_field('course_image'), 'large' ); ?>
-					<?php endif; ?>
+							<?php if( get_field('course_image') ): 
+									echo wp_get_attachment_image( get_field('course_image'), 'large' ); ?>
+							<?php endif; ?>
 
-				<?php endif; ?>
-					
-			<?php endwhile; ?>
-				
+						<?php endif; ?>		
+					</div> <!---end course wrap--->
+				<?php endwhile; ?>
+				</section>
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
