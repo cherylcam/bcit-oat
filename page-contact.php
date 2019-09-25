0ff01c13f179110ff6e42cca85efd95249356cc3
Matthew Yuen <A01185565@ad.bcit.ca>
2019-09-25 12:54:45 -0700

    contact page

--- page-contact.php
+++ page-contact.php
@@ -25,26 +25,28 @@ get_header();
 
 			while ( have_rows('faculty_staff') ) : the_row();			
 		?>
+		
+		<h2>Faculty Contact</h2>
 			<div class="faculty-staff">
-				<h2>Faculty Contact</h2>
-				<p>Name:
+				
+				<p class="faculty-name">
 					<?php
-						echo '<b class="faculty-name">' . the_sub_field('faculty_name') . '</b>';
+						echo  the_sub_field('faculty_name');
 					?>
 			  	</p>
-				<p> Role: 
+				<p class="faculty-position">
 					<?php
-						echo '<b class="faculty-position">' . the_sub_field('faculty_position') . '</b>';
+						echo the_sub_field('faculty_position');
 					?>
 				</p>
-				<p> Number: 
+				<p class="faculty-number">
 					<?php
-						echo '<b class="faculty-number">' . the_sub_field('faculty_number') . '</b>';
+						the_sub_field('faculty_number');
 					?>
 				</p>
-				<p> Email: 
+				<p class="faculty-email">
 					<?php
-						echo '<b class="faculty-email">' . the_sub_field('faculty_email') . '</b>';
+						echo the_sub_field('faculty_email');
 					?>
 				</p>
 			</div>	
@@ -57,43 +59,54 @@ get_header();
 			while ( have_rows('program_info') ) : the_row();			
 		?>
 
+		<h2>Program Location</h2>
 			<div class="program-location">
-				<h2>Program Location</h2>
-				<p>Program Name:
+	
+				<p class="program-name">
 					<?php
-						echo '<b class="program-name">' . the_sub_field('program_name') . '</b>';
+						echo  the_sub_field('program_name');
 					?>
 			  	</p>
-				<p> Location: 
+				<p class="program-location">
 					<?php
-						echo '<b class="program-location">' . the_sub_field('program_location') . '</b>';
+						echo the_sub_field('program_location');
 					?>
 				</p>
-				<p> Number: 
+				<p class="program-phone">
 					<?php
-						echo '<b class="program-phone">' . the_sub_field('program_phone') . '</b>';
+						echo the_sub_field('program_phone');
 					?>
 				</p>
-				<p> Email: 
+				<p class="program-email">
 					<?php
-						echo '<b class="program-email">' . the_sub_field('program_email') . '</b>';
+						echo the_sub_field('program_email');
 					?>
 				</p>
-				<p> Website: 
-					<?php
-						echo '<b class="program-website">' ?> 
-						<a href=" <?php echo  the_sub_field('program_website') ?> " target="_blank"><?php echo the_sub_field('program_website') ?></a></b>
+				<p class="program-website">
+						<a href=" <?php echo  the_sub_field('program_website'); ?> " target="_blank"><?php echo the_sub_field('program_website'); ?></a>
 				</p>
-				<p> BCIT: 
-					<?php
-						echo '<b class="bcit-website">' ?> 
-						<a href=" <?php echo  the_sub_field('bcit_website') ?> " target="_blank"><?php echo the_sub_field('bcit_website') ?></a></b>
+				<p class="bcit-website">
+						<a href=" <?php echo  the_sub_field('bcit_website'); ?> " target="_blank"><?php echo the_sub_field('bcit_website'); ?></a>
 				</p>
 			</div>	
 
 			<div class="bcit-map">
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2602.6312010021024!2d-123.11752518431048!3d49.28338507933136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54867178c92a69b9%3A0x6b40938af55472d6!2s555+Seymour+St%2C+Vancouver%2C+BC+V6B+3H6!5e0!3m2!1sen!2sca!4v1566424196502!5m2!1sen!2sca" width="600" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
 			</div>
+			
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
 
 		<?php
 		
