3dc13c3eb5c67ef6893348b75a3d2947c8aa8dcc
Matthew Yuen <A01185565@ad.bcit.ca>
2019-09-25 11:24:55 -0700

    page contact

--- page-contact.php
+++ page-contact.php
@@ -17,17 +17,16 @@ get_header();
 
 
 		<?php
-
 			if( have_rows('faculty_staff') ):
 
 				echo '<h1>';
 				the_title();
 				echo '</h1>';
 
-			while ( have_rows('faculty_staff') ) : the_row();
-			
+			while ( have_rows('faculty_staff') ) : the_row();			
 			?>
 			<div class="staff-container">
+				<h1>Faculty Contact</h1>
 				<p>Name:
 					<?php
 						echo '<b class="schedule-title">' . the_sub_field('faculty_name') . '</b>';
@@ -48,10 +47,10 @@ get_header();
 						echo '<b>' . the_sub_field('faculty_email') . '</b>';
 					?>
 				</p>
-
-
-
 			</div>	
+			<div class="bcit-map">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2602.6312010021024!2d-123.11752518431048!3d49.28338507933136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54867178c92a69b9%3A0x6b40938af55472d6!2s555+Seymour+St%2C+Vancouver%2C+BC+V6B+3H6!5e0!3m2!1sen!2sca!4v1566424196502!5m2!1sen!2sca" width="600" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+			</div>
 
 			<?php
 
