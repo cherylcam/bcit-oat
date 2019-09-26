fbd0e450ea4f81a172c6a31f9d845cb842b9a5ed
Cheryl Cam <cherylcam97@gmail.com>
2019-09-26 16:20:18 -0700

    Updated header and added theme-font

--- header.php
+++ header.php
@@ -26,22 +26,12 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$bcit_oat_description = get_bloginfo( 'description', 'display' );
-			if ( $bcit_oat_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $bcit_oat_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+			<h1 class="site-title">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">OAT</a>
+			</h1>
+			<div class="site-description">
+				<p>OFFICE ADMINISTRATOR <br> WITH TECHNOLOGY PROGRAM</p>
+			</div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
