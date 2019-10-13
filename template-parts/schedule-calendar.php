6e5528daae092afa3cfe0938d2d48aa2057cfb4c
mariusgessler <marius.gessler@gmail.com>
2019-10-12 20:40:42 -0700

    Update schedule-calendar.php

--- template-parts/schedule-calendar.php
+++ template-parts/schedule-calendar.php
@@ -13,19 +13,14 @@ while ($query->have_posts()):
 	$table_data 	= json_decode(get_the_content()); // Parsing the json into an object.
 	$allMonths = array();
 
-
-	foreach ($table_data as $item):
-		$month = date("F", strtotime($item[1]));//Format the month to text
-		$year = date("Y", strtotime($item[1])); // Get the year in the right format
-		array_push($allMonths, $month);	// Push all months in an array --> It going to add the month info for every day
-		$schedule[$month][] = $item; //Add the month as a key to the array
-	endforeach;
+		foreach ($table_data as $item):
+			$month = date("F", strtotime($item[1]));//Format the month to text
+			$year = date("Y", strtotime($item[1])); // Get the year in the right format
+			array_push($allMonths, $month);	// Push all months in an array --> It going to add the month info for every day
+			$schedule[$month][] = $item; //Add the month as a key to the array
+		endforeach;
 ?>
 		<div class="schedule-calendar">
-				
-
-		
-		
 			<?php $months = array_unique($allMonths); // Filter out all the duplicate values
 			// print_r($month);
 			$numberOfDaysInMonth = array(); //Empty array to get the number of days in a month	
@@ -38,9 +33,6 @@ while ($query->have_posts()):
 					$monthAsNumber = date("n", strtotime($month)); // Converting the months to a number so they can be used in cal_days_in_month function
 					array_push($numberOfDaysInMonth, cal_days_in_month(CAL_GREGORIAN, $monthAsNumber, $year)); // Push the number of days for each month in an array
 					$firstDay = date("N",  mktime(0, 0, 0, $monthAsNumber, 1, $year)); // Getting the first weekday of month.?>
-				
-
-					<!-- <div class="swiper-slide" slide_index=<?php echo $monthIndex?>>  -->
 						<div class="schedule-header">
 							<h1><?php echo $month . " " . $year ?> </h1>
 						</div>
@@ -58,7 +50,7 @@ while ($query->have_posts()):
 								<?php $dayOfMonth = 1 ?>
 								<div class="date-grid">
 									<?php for ($i = 0 ; $i < 5; $i ++): // Create table rows?> 
-											<?php for ($j = 0; $j < 7; $j++ ): //Creating the individual columns ?>  
+											<?php for ($j = 0; $j < 7; $j++ ): //Creating the individual cells ?>  
 												<?php if ($i == 0 && $j < $firstDay - 1):  ?>
 													<div class="beginning-month">
 													</div>
@@ -100,9 +92,7 @@ while ($query->have_posts()):
 										
 									<?php endfor; ?>
 									</div><!-- END date-grid -->
-								
-						
-					</table>
+						</table>
 					</div>
 					<!-- </div> 	END SWIPERSLIDE -->
 					<?php $monthIndex++ ?>
