cff3ac5e207ca2ef8103e0981cca5fe245e95bd9
chanroyc <chanroyc@gmail.com>
2019-10-14 20:26:08 -0700

    Updated Careers Page

--- page-careers.php
+++ page-careers.php
@@ -19,60 +19,55 @@ get_header();
 		<main id="main" class="site-main">
 		
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<header class="page-header">
+			<h1 class="page-title">Careers</h1>
+		</header><!-- .page-header -->
+		
+		<div class='careers-wrapper'>
+			<h1>Civic Jobs BC</h1>
+			<div class='jobPostings'>
+			<?php
+				$url = "https://www.civicjobs.ca/rss/pc?id=36"; // Civic Jobs Office Administration
+				$feeds = file_get_contents($url);
+				$rss = simplexml_load_string($feeds);
+				$number_of_posts_to_show = 6;
 
-			get_template_part( 'template-parts/content', 'page' );
+				$items = [];
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				foreach($rss->channel->item as $entry):
+					$items[] = [
+						'title' 		=> $entry->title,			// Title
+						'link' 			=> $entry->link,			// Link
+						'description' 	=> $entry->description,		// Description
+						'pubDate' 		=> $entry->pubDate,			// Publication Date
+					];
+				endforeach;
 
-		endwhile; // End of the loop.
-		?>
+				$counter = 0;
+				foreach($items as $item):
+					/* Show a set number of most recent Posts */
+					if($counter === $number_of_posts_to_show):
+						break;
+					endif;
 
-		<?php
-			$url = "https://www.civicjobs.ca/rss/pc?id=36"; // Civic Jobs Office Administration
-			$feeds = file_get_contents($url);
-			$rss = simplexml_load_string($feeds);
-			$number_of_posts_to_show = 4;
-
-			$items = [];
-
-			foreach($rss->channel->item as $entry):
-				$items[] = [
-					'title' 		=> $entry->title,			// Title
-					'link' 			=> $entry->link,			// Link
-					'description' 	=> $entry->description,		// Description
-					'pubDate' 		=> $entry->pubDate,			// Publication Date
-				];
-			endforeach;
-
-			$counter = 0;
-			foreach($items as $item):
-				/* Show a set number of most recent Posts */
-				if($counter === $number_of_posts_to_show):
-					break;
-				endif;
-
-				/* Parse XML Object into Usable String */
-				$title = $item['title']->__toString();
-				$link = $item['link']->__toString();
-				$description = $item['description']->__toString();
-				$pubDate = $item['pubDate']->__toString();
-		?>
-			<div class='jobPosting'>
-				<a class='title' href='<?php print_r($link); ?>'><h1><?php print_r($title); ?></h1></a>
-				<span class='pubDate'><?php print_r($pubDate); ?></span>
-				<p class='description'><?php print_r($description); ?></p>
-				<a class='viewJob' href='<?php print_r($link); ?>'>View Posting</a>
+					/* Parse XML Object into Usable String */
+					$title = $item['title']->__toString();
+					$link = $item['link']->__toString();
+					$description = $item['description']->__toString();
+					$pubDate = $item['pubDate']->__toString();
+			?>
+					<div class='posting'>
+						<h2 class='title'><a href='<?php print_r($link); ?>'><?php print_r($title); ?></a></h2>
+						<p class='pubDate'>Posted on <?php print_r($pubDate); ?></p>
+						<p class='description'><?php print_r($description); ?></p>
+						<p class='viewJob'><a href='<?php print_r($link); ?>'>View Posting</a><p>
+					</div>
+			<?php
+					$counter++;
+				endforeach;
+			?>
 			</div>
-		<?php
-				$counter++;
-			endforeach;
-		?>
+		</div>
 	
 
 		</main><!-- #main -->
