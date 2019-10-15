590116af13e8dcc9f0a8b9f51b84eb16d60ecb79
chanroyc <chanroyc@gmail.com>
2019-10-15 02:08:19 -0700

    Reverted Footer

--- footer.php
+++ footer.php
@@ -8,66 +8,61 @@
  *
  * @package bcit-oat
  */
-
 ?>
 
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="footer-headings-wrap">
-			<div class="footer-headings">
-				<div class='footer-headings-row'>
-						<h3>Students</h3>
-						<h3>Program</h3>
-						<h3>Find Us</h3>
-				</div>
-			</div>
-		<div>
-		
-		<div class="footer-links-wrap">	
-			<div class='footer-links'>
-
-				<img src="http://bcitoat.bcitwebdeveloper.ca/wp-content/uploads/2019/10/bcit_cmyk-logo.png">
-			
-				<div class='footer-section'>
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'footer-students',
-							'menu_id'        => 'footer-students',
-						) );
-					?>
-				</div>
-				
-				<div class='footer-section'>
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'footer-program',
-							'menu_id'        => 'footer-program',
-						) );
-					?>
-				</div>
-
-				<div class='footer-section'>
-					<?php
-						if(function_exists('the_field')):
-					?>
-							<p><?php the_field('company_name', 'option'); ?> <p>
-							<p><?php the_field('street_address', 'option'); ?><p>
-							<p><?php the_field('city', 'option'); ?>, <?php the_field('province', 'option'); ?></p>
-							<p><?php the_field('zip_code', 'option'); ?></p>
-							<br />
-							<p>Telephone: <?php the_field('telephone', 'option') ?></p>
-							<p>Toll-free: <?php the_field('toll_free', 'option') ?></p>
-					<?php
-						endif;
-					?>
-
-					<br>
-					<a href='https://www.bcit.ca/contacts/'>More Contact Numbers</a>
+		<div class="footer-headings">
+			<div class='footer-headings-row'>
+					<h3>Students</h3>
+					<h3>Program</h3>
+					<h3>Find Us</h3>
 			</div>
 		</div>
+
+		<div class='footer-links'>
+
+			<img src="http://bcitoat.bcitwebdeveloper.ca/wp-content/uploads/2019/10/bcit_cmyk-logo.png">
+		
+			<div class='footer-section'>
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'footer-students',
+						'menu_id'        => 'footer-students',
+					) );
+				?>
+			</div>
+			
+			<div class='footer-section'>
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'footer-program',
+						'menu_id'        => 'footer-program',
+					) );
+				?>
+			</div>
+
+			<div class='footer-section'>
+				<?php
+					if(function_exists('the_field')):
+				?>
+						<p><?php the_field('company_name', 'option'); ?> <p>
+						<p><?php the_field('street_address', 'option'); ?><p>
+						<p><?php the_field('city', 'option'); ?>, <?php the_field('province', 'option'); ?></p>
+						<p><?php the_field('zip_code', 'option'); ?></p>
+						<br />
+						<p>Telephone: <?php the_field('telephone', 'option') ?></p>
+						<p>Toll-free: <?php the_field('toll_free', 'option') ?></p>
+				<?php
+					endif;
+				?>
+
+				<br>
+				<a href='https://www.bcit.ca/contacts/'>More Contact Numbers</a>
+			</div>
 	</footer><!-- #colophon -->
-	<div class="copyright-wrap"><span class='copyright'>Copyright &copy; BCIT OAT 2019. All rights reserved.</span></div>
+	<span class='copyright'>Copyright &copy; BCIT OAT 2019. All rights reserved.</span>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
