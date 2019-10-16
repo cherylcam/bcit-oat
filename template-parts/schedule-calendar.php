891b043a1cf9435cb2dcc31e12bdcb6fd99bc059
mariusgessler <marius.gessler@gmail.com>
2019-10-16 09:53:00 -0700

    put sunday and saturday at beginning/end of week

--- template-parts/schedule-calendar.php
+++ template-parts/schedule-calendar.php
@@ -21,36 +21,44 @@ while ($query->have_posts()):
 			$schedule[$month][] = $item; //Add the month as a key to the array
 		endforeach;
 		// print_r($schedule["April"]);
-	?>		
+
+?>
+		
+		
 		<?php $months = array_unique($allMonths); // Filter out all the duplicate values
 		// print_r($month);
 		$numberOfDaysInMonth = array(); //Empty array to get the number of days in a month	
 		$monthIndex = 0; // Define index so you can loop through all months 
 		?>
 
+			
+		
+			
 		<div class="schedule-calendar">
 				<button class="goto"><span>Go to today</span></button>
 			<?php foreach ($months as $month):
 					$monthAsNumber = date("n", strtotime($month)); // Converting the months to a number so they can be used in cal_days_in_month function
 					array_push($numberOfDaysInMonth, cal_days_in_month(CAL_GREGORIAN, $monthAsNumber, $year)); // Push the number of days for each month in an array
-					$firstDay = date("N",  mktime(0, 0, 0, $monthAsNumber, 1, $year)); // Getting the first weekday of month. Number between 1 - 6?>
+					$firstDay = date("w",  mktime(0, 0, 0, $monthAsNumber, 1, $year)); // Getting the first weekday of month. Number between 1 - 6?>
 
 						<div class="schedule-header" id=<?php echo $month?>>
 							<h1><?php echo $month . " " . $year ?> </h1>
 						</div>
 						<div class="calendar">
 							<div class="weekdays-grid">
+								<p class="weekday">Sunday</p>
 								<p class="weekday">Monday</p>
 								<p class="weekday">Tuesday</p>
 								<p class="weekday">Wednesday</p>
 								<p class="weekday">Thursday</p>
 								<p class="weekday">Friday</p>
+								<p class="weekday">Saturday</p>
 							</div>
 								<?php $dayOfMonth = 1 ?>
 								<div class="date-grid">
 									<?php for ($i = 0 ; $i < 5; $i ++): // Create rows?> 
-											<?php for ($j = 0; $j < 5; $j++ ): //Creating the individual cells ?>  
-												<?php if ($i == 0 && $j < $firstDay - 1):  ?> 
+											<?php for ($j = 0; $j < 7; $j++ ): //Creating the individual cells ?>  
+												<?php if ($i == 0 && $j < $firstDay ):  ?> 
 													<div class="beginning-month">
 													</div>
 												<?php elseif ($dayOfMonth > $numberOfDaysInMonth[$monthIndex] || $dayOfMonth > count($schedule[$month])): ?>
