386c5f6a81c667c4786c3a37ac28fe9a8b9ab15a
Cheryl Cam <cherylcam97@gmail.com>
2019-10-17 14:56:32 -0700

    Create custom-admin-menu.php

---
+++ inc/custom-admin-menu.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Customize Admin Menu
+ */
+
+//Activate Custom menu order & The order of the menus is top to bottom!
+function custom_menu_order($menu_ord) {
+	if (!$menu_ord) return true;
+	return array(
+		'index.php', // Dashboard
+		'edit.php?post_type=page', // Pages
+		'upload.php', // Media
+		'themes.php', // Appearance
+		'plugins.php', // Plugins
+		'users.php', // Users
+		'tools.php', // Tools
+		'options-general.php', // Settings
+	);
+}
+add_filter('custom_menu_order', 'custom_menu_order');
+add_filter('menu_order', 'custom_menu_order');
+
+//RESTRICTED FOR specific USERS
+
+function remove_menus(){
+	remove_menu_page( 'edit.php' );           //Posts
+	remove_menu_page( 'edit-comments.php' );  //Comments
+}
+add_action('admin_menu', 'remove_menus');
