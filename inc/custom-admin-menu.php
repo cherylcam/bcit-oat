eae5a4445d395d810fdfeee5c85b16a783a52dfb
Cheryl Cam <cherylcam97@gmail.com>
2019-10-17 15:10:11 -0700

    Update custom-admin-menu.php

--- inc/custom-admin-menu.php
+++ inc/custom-admin-menu.php
@@ -7,14 +7,18 @@
 function custom_menu_order($menu_ord) {
 	if (!$menu_ord) return true;
 	return array(
-		'index.php', // Dashboard
-		'edit.php?post_type=page', // Pages
-		'upload.php', // Media
-		'themes.php', // Appearance
-		'plugins.php', // Plugins
-		'users.php', // Users
-		'tools.php', // Tools
-		'options-general.php', // Settings
+		'index.php',                              // Dashboard
+        'edit.php?post_type=page',                // Pages
+        'tablepress',                             // schedule
+        'edit.php?post_type=oat-courses',         // Courses
+        'edit.php?post_type=oat-certifications',  // Certifications
+        'wpcf7',                                  // Contact Form
+		'upload.php',                             // Media
+		'themes.php',                             // Appearance
+		'plugins.php',                            // Plugins
+		'users.php',                              // Users
+		'tools.php',                              // Tools
+		'options-general.php',                    // Settings
 	);
 }
 add_filter('custom_menu_order', 'custom_menu_order');
